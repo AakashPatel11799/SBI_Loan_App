ba9c679bf2be21ce430d5512537c15c717e44ef7
Aakash Patel <aakashpatelsky11@gmail.com>
2024-10-16 12:58:47 +0530

    my message

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -5,8 +5,6 @@ package in.ashokit;
  */
 public class App {
     public static void main(String[] args) {
-        int x=10;
-        int y=20;
         System.out.println("Hello World!");
     }
 }
--- src/test/java/in/ashokit/AppTest.java
+++ src/test/java/in/ashokit/AppTest.java
@@ -14,7 +14,6 @@ public class AppTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
-        int z=10;
         assertTrue(true);
     }
 }
